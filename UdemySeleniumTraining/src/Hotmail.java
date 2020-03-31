0d4ee1c9381dc0d662609e386ef76654df107611
Josue Chavez Duran <josuechavezduran@hotmail.com>
2020-03-31 16:40:29 -0700

    Demo renamed for Hotmail

--- UdemySeleniumTraining/src/Hotmail.java
+++ UdemySeleniumTraining/src/Hotmail.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class Demo {
+public class Hotmail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
